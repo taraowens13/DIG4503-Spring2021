6cac324ac0ec9cdb2e059a69c9ff3b70fd4c1b24
taraowens13 <74627961+taraowens13@users.noreply.github.com>
2021-01-29 01:20:21 -0500

    Lab 2 Edits

--- lab2/index.js
+++ lab2/index.js
@@ -3,5 +3,5 @@ import Fetch from './Fetch.js';
 const valid = new Fetch("316" , "#90ee90");
 valid.fetch();
 
-const invalid = new Fetch("cat");
+const invalid = new Fetch("dragon");
 invalid.fetch();
