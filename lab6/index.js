ce156e2b7aba5d965454c67e76681f5789c5cd54
taraowens13 <74627961+taraowens13@users.noreply.github.com>
2021-02-25 11:22:10 -0500

    lab6

---
+++ lab6/index.js
@@ -0,0 +1,22 @@
+import Express from 'express';
+
+const App = Express();
+const port = 45030;
+
+
+const person = {
+    name: "Tara",
+    color: "Blue"
+};
+
+App.get("/", function(req, res) {
+    res.send("Hello World!");
+});
+
+App.get("/person2", function(req, res) {
+    res.json(person);
+});
+
+App.listen(port, function(){
+    console.log("Server is running!");
+});
