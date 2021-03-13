7a82c6c8957a50edadff97341c3d6e62c61084b3
taraowens13 <74627961+taraowens13@users.noreply.github.com>
2021-03-13 16:36:51 -0500

    installed nodemon

--- lab7/index.js
+++ lab7/index.js
@@ -6,24 +6,16 @@ const port = 45030;
 const arrayExample = [
     "Hello!",
     "Hey!",
-    "Hi!",
     "Howdy!"
 ];
 
-App.get("/:id", (req, res) => {
+App.get("/collection/:resource", (req, res) => {
 
     let result = "Not found!";
 
-    arrayExample.forEach((value) => {
-        if(req.params.id ==  value){
-            result = value;
-        }
-    });
-
     res.json(result);
-
 });
 
 App.listen(port, () => {
     console.log("Server running!");
-})
+});
