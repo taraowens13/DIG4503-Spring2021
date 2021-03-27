74b55ab168473aa4c2517347ab8aaf3324f62213
taraowens13 <74627961+taraowens13@users.noreply.github.com>
2021-03-27 17:05:20 -0400

    lab9

--- lab9/index.js
+++ lab9/index.js
@@ -1,19 +1,23 @@
 import MongoClient from 'mongodb';
 
-const URL = "mongodb+srv://TaraOwens:O8X7O6lW9ftvZ3Rs@cluster0.yuzwq.mongodb.net"
+const URL = "mongodb+srv://TaraOwens:O8X7O6lW9ftvZ3Rs@cluster0.yuzwq.mongodb.net";
 
 MongoClient.connect(URL, { useUnifiedTopology: true })
-.then(connection => {
-    let database = connection.db();
-    let collection = database.collection();
-    let cursor = collection.find({year: {$gt: 2010}, rated: "R"});
+    .then(connection => {
+        let database = connection.db("sample_airbnb");
 
-    cursor.forEach(document => {
-        console.log(document.title);
-    }, () => {
-        connection.close();
+        let collection = database.collection("listingsAndReviews");
+        
+        let cursor = collection.find({
+            beds: { $gte: 5 },
+            price: { $lte: 200 },
+            "review_scores.review_scores_rating": { $gte: 99 },
+        });
+
+        cursor.forEach(document => {
+                console.log(document);
+            });
+    })
+    .catch(error => {
+        console.log("Error: " + error);
     });
-})
-.catch(error => {
-    console.log("Error " + error);
-});
